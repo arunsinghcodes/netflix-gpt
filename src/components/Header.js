1d341c877e1fddb7bdd9353be4897ea6d7d8af45
Arun Singh <arunsinghsnd@gmail.com>
2026-02-21 15:58:38 +0530

    Add: login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import NetflixLogo from "../assets/Netflix_Logo_PMS.png"
 
 const Header = () =>{
     return(
-        <div>
-            <h1>Header</h1>
+        <div className="w-full absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+            <img className="w-44" src={NetflixLogo} alt="logo"/>
         </div>
     )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,52 @@
-const Login = () =>{
-    return(
-        <div>
-            <h1>Login Page</h1>
-        </div>
-    )
-}
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/37372b0c-16ef-4614-9c66-f0464ffe4136/web/IN-en-20260216-TRIFECTA-perspective_74aa38a5-f527-417e-a604-a039567a350b_large.jpg"
+          srcSet="
+        https://assets.nflxext.com/ffe/siteui/vlv3/37372b0c-16ef-4614-9c66-f0464ffe4136/web/IN-en-20260216-TRIFECTA-perspective_74aa38a5-f527-417e-a604-a039567a350b_large.jpg  2000w,
+        https://assets.nflxext.com/ffe/siteui/vlv3/37372b0c-16ef-4614-9c66-f0464ffe4136/web/IN-en-20260216-TRIFECTA-perspective_74aa38a5-f527-417e-a604-a039567a350b_medium.jpg 1279w,
+        https://assets.nflxext.com/ffe/siteui/vlv3/37372b0c-16ef-4614-9c66-f0464ffe4136/web/IN-en-20260216-TRIFECTA-perspective_74aa38a5-f527-417e-a604-a039567a350b_small.jpg   959w
+      "
+          alt=""
+          aria-hidden="true"
+          className="default-ltr-iqcdef-cache-19j6xtr"
+        />
+      </div>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-full md:w-6/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Full Name"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        {/* <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p> */}
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">Sign In</button>
+        {/* <p className="py-4 cursor-pointer">
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now."}
+        </p> */}
+      </form>
+    </div>
+  );
+};
 
 export default Login;
