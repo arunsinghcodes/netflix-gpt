236920dce87c975e66d8f13c95ea6a1cb4c545b5
Arun Singh <arunsinghsnd@gmail.com>
2026-02-21 16:13:31 +0530

    add: login page made generic for signup and sign in

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Header />
       <div className="absolute">
         <img
+          // className="h-screen object-cover"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/37372b0c-16ef-4614-9c66-f0464ffe4136/web/IN-en-20260216-TRIFECTA-perspective_74aa38a5-f527-417e-a604-a039567a350b_large.jpg"
           srcSet="
         https://assets.nflxext.com/ffe/siteui/vlv3/37372b0c-16ef-4614-9c66-f0464ffe4136/web/IN-en-20260216-TRIFECTA-perspective_74aa38a5-f527-417e-a604-a039567a350b_large.jpg  2000w,
@@ -14,19 +22,22 @@ const Login = () => {
       "
           alt=""
           aria-hidden="true"
-          className="default-ltr-iqcdef-cache-19j6xtr"
         />
       </div>
       <form
         onSubmit={(e) => e.preventDefault()}
         className="w-full md:w-6/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
       >
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
-        <input
-          type="text"
-          placeholder="Full Name"
-          className="p-4 my-4 w-full bg-gray-700"
-        />
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
         <input
           type="text"
           placeholder="Email Address"
@@ -38,12 +49,14 @@ const Login = () => {
           className="p-4 my-4 w-full bg-gray-700"
         />
         {/* <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p> */}
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">Sign In</button>
-        {/* <p className="py-4 cursor-pointer">
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
           {isSignInForm
             ? "New to Netflix? Sign Up Now"
             : "Already registered? Sign In Now."}
-        </p> */}
+        </p>
       </form>
     </div>
   );
