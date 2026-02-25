34a299c7ce01cd8b4b652cf481e4ee904f000ffd
Arun Singh <arunsinghsnd@gmail.com>
2026-02-26 00:52:47 +0530

    Add: the store login done and redirect to browse page

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,13 +1,13 @@
 import React from "react";
+import Header from "./Header";
 
-const Browse = () =>{
-    return(
-        <div>
-        Browse Page
-        </div>
-
-    )
-} 
-
+const Browse = () => {
+  return (
+    <div>
+      <Header />
+      Browse Page
+    </div>
+  );
+};
 
 export default Browse;
--- src/components/Login.js
+++ src/components/Login.js
@@ -6,10 +6,13 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 //? useRef:- useRef is a React Hook that lets you reference a value that's not needed for rendering
 
 const Login = () => {
+   const navigate = useNavigate();
+
   const [isSignInForm, setIsSignInForm] = useState(() => {
     const saved = localStorage.getItem("authMode");
     return saved ? saved === "signin" : true;
@@ -44,6 +47,7 @@ const Login = () => {
         .then((userCredential) => {
           const user = userCredential.user;
           console.log(user);
+          navigate("/browse")
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -56,6 +60,7 @@ const Login = () => {
         .then((userCredential) => {
           const user = userCredential.user;
           console.log(user);
+           navigate("/browse")
         })
         .catch((error) => {
           const errorCode = error.code;
