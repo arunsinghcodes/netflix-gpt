787bd408a0c3a44f097ee7f6ec914590c5a2a4a4
Arun Singh <arunsinghsnd@gmail.com>
2026-02-28 23:04:54 +0530

    Add: TMDB api keys

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
+import { API_OPTIONS } from "../utils/contants";
 
 const Browse = () => {
+  const url = "https://api.themoviedb.org/3/movie/now_playing?page=1";
+
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(url, API_OPTIONS);
+    const json = await data.json();
+
+    console.log(json?.results);
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
   return (
     <div>
       <Header />
