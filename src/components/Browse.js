8d6115e9b3d90ffd1539ce13eee42f5c12630b07
Arun Singh <arunsinghsnd@gmail.com>
2026-02-28 23:30:47 +0530

    Add: nowPlayingMovie data added to the store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,19 +1,24 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/contants";
+import { API_OPTIONS, BASE_URL } from "../utils/contants";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovie } from "../utils/movieSlice";
 
 const Browse = () => {
-  const url = "https://api.themoviedb.org/3/movie/now_playing?page=1";
+  const dispatch = useDispatch();
+  const url = BASE_URL + "movie/now_playing?page=1";
 
   const getNowPlayingMovies = async () => {
     const data = await fetch(url, API_OPTIONS);
     const json = await data.json();
 
     console.log(json?.results);
+    dispatch(addNowPlayingMovie(json?.results));
   };
 
   useEffect(() => {
     getNowPlayingMovies();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <div>
