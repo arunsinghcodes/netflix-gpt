b71dc668db8d96cb9082ad207765533a5846ce25
Arun Singh <arunsinghsnd@gmail.com>
2026-03-01 23:42:08 +0530

    Added the Videotitle compoent to the main conatiner

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import VideoTitle from "./VideoTitle";
+import VideoBackground from "./VideoBackground";
 
 const MainContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
@@ -9,9 +11,12 @@ const MainContainer = () => {
   console.log(movies);
   const mainMovie = movies[0];
   console.log(mainMovie);
+
+  const {original_title, overview } = mainMovie;
   return (
     <div>
-      <h1>MainContainer</h1>
+      <VideoTitle title={original_title} overview={overview}/>
+      <VideoBackground/>
     </div>
   );
 };
