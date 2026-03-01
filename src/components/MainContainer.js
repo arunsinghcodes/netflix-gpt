9aa2e94544c41a984beeda782876fb6316cffed4
Arun Singh <arunsinghsnd@gmail.com>
2026-03-02 00:10:11 +0530

    Added the browse section

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -12,11 +12,11 @@ const MainContainer = () => {
   const mainMovie = movies[0];
   console.log(mainMovie);
 
-  const {original_title, overview } = mainMovie;
+  const {original_title, overview, id } = mainMovie;
   return (
-    <div>
+    <div className="pt-[30%] bg-black md:pt-0">
       <VideoTitle title={original_title} overview={overview}/>
-      <VideoBackground/>
+      <VideoBackground movieId={id}/>
     </div>
   );
 };
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { API_OPTIONS, BASE_URL } from "../utils/contants";
+import { addTrailerVideo } from "../utils/movieSlice";
 
-const VideoBackground = () => {
+const VideoBackground = ({ movieId }) => {
+  const dispatch = useDispatch();
+
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+
+  const getMovieVideos = async () => {
+    const url = BASE_URL + `movie/${movieId}/videos`;
+
+    const data = await fetch(url, API_OPTIONS);
+    const json = await data.json();
+
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    dispatch(addTrailerVideo(trailer));
+  };
+
+  useEffect(() => {
+    !trailerVideo && getMovieVideos();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div>
-      VideoBackground
+    <div className=" w-screen">
+      <iframe
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideo?.key +
+          "?&autoplay=1&mute=1"
+        }
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
     </div>
-  )
-}
+  );
+};
 
-export default VideoBackground
+export default VideoBackground;
